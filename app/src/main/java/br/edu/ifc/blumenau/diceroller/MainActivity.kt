cfd1f130e359e662a522a0557a00665b26b1a031
Mateus Bueno <mateusmobueno@gmail.com>
2019-08-23 16:45:53 -0300

    Dice rolling with random text numbers through a button.

--- app/src/main/java/br/edu/ifc/blumenau/diceroller/MainActivity.kt
+++ app/src/main/java/br/edu/ifc/blumenau/diceroller/MainActivity.kt
@@ -2,11 +2,28 @@ package br.edu.ifc.blumenau.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        val randomInt = Random().nextInt(6)+1
+
+        resultText.text = randomInt.toString()
     }
 }
